0d4dc70ac4f01361fcb50a41a8ddf3cf1c0ce489
AudreyHal <chiomahalim@gmail.com>
2024-01-28 22:55:45 +0100

    Implement color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('black');
+
+  const handleClick= async()=>{
+  const [tab] = await chrome.tabs.query({active: true});
+  chrome.scripting.executeScript<string[], void>({
+    target: {tabId: tab.id!},
+    args : [color],
+    func: (color ) =>{
+        document.body.style.backgroundColor =  color
+    }
+  })
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Background Color Changer</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleClick} className='btn'>
+          Apply Color
         </button>
+        <input type='color' name="color" onChange={e=>setColor(e.currentTarget.value)} value={color} />
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+         Select a color then click on the button to apply on background
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
